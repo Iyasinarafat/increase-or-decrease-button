18026d5d9b66b31bd11682544fc8b7912223aa6d
Iyasinarafat <iyasinarafat757@gmail.com>
2024-09-01 02:06:03 +0600

    second commit

--- script.js
+++ script.js
@@ -3,13 +3,32 @@ const plusbtn = document.getElementById('plusbtn');
 const minusbtn = document.getElementById('minusbtn');
 
 let counte = 0;
-plusbtn.addEventListener('click', () =>{
-    counte = counte + 1;
+const updateCounter = (value) => {
+    counte = counte + value;
     counter.textContent = counte;
+
+    if( counte >= 10){
+        plusbtn.setAttribute('disabled', true)
+    }
+    else{
+        plusbtn.removeAttribute('disabled', false)
+    }
+    //minus button code:
+    
+    if(counte <= 0){
+        minusbtn.setAttribute('disabled', true)
+    }
+    else{
+        minusbtn.removeAttribute('desabled',false)
+    }
+
+}
+plusbtn.addEventListener('click', () =>{
+    updateCounter(+1);
 })
 minusbtn.addEventListener('click', () => {
-    counte = counte - 1;
-    counter.textContent = counte;
+    updateCounter(-1);
+    
 
 
 })
